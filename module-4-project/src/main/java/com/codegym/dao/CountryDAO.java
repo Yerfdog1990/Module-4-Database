29290fe70926736f6565b8aa2f047c1fd1c38459
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-06-20 20:45:25 +0700

    Chore: Add data access object They provide a clean, organized, and abstract layer for accessing the database in an application.

---
+++ module-4-project/src/main/java/com/codegym/dao/CountryDAO.java
@@ -0,0 +1,19 @@
+package com.codegym.dao;
+
+import com.codegym.domain.Country;
+import java.util.List;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+public class CountryDAO {
+    private final SessionFactory sessionFactory;
+
+    public CountryDAO(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public List<Country> getAll() {
+        Query<Country> query = sessionFactory.getCurrentSession().createQuery("select c from Country c", Country.class);
+        return query.list();
+    }
+}
